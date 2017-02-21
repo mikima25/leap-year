52c72d0bedd92a5e02b98c79fb09e6f282a94cc5
mikima25 <vtalism20@gmail.com>
2017-02-21 16:51:07 +0300

    divisible 400 specs

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,10 @@ var leapYear = function(year) {
         return false;
       }
     };
+var leapYear = function(year) {
+    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+          return true;
+        } else {
+          return false;
+        }
+      };
